f22dabc0e76d8a9d6cf029a1e5ef4be5e3356d17
starkoracia <starkoracia@gmail.com>
2023-10-10 02:41:58 +0100

    Finished ArraysMinMaxApp

--- info_arrays_task_4/src/main/java/com/starkoracia/arrays/ArraysMinMaxApp.java
+++ info_arrays_task_4/src/main/java/com/starkoracia/arrays/ArraysMinMaxApp.java
@@ -1,13 +1,46 @@
 package com.starkoracia.arrays;
 
+import java.util.Arrays;
 import java.util.random.RandomGenerator;
 
 public class ArraysMinMaxApp {
+
     public static void main(String[] args) {
-        RandomGenerator randomGenerator = RandomGenerator.of("L128X1024MixRandom");
-        for (int i = 0; i < 100; i++) {
-            int nextInt = randomGenerator.nextInt(100);
-            System.out.println(nextInt);
-        }
+        Integer[] integers = generateIntArray(5);
+        System.out.println(Arrays.toString(integers));
+
+        findAndPrintMinMaxFirstWay(integers);
+        findAndPrintMinMaxSecondWay(integers);
     }
+
+    private static void findAndPrintMinMaxFirstWay(Integer[] integers) {
+        int min = integers[0];
+        int max = integers[0];
+        for (int num : integers) {
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+        System.out.printf("Min = %d; Max = %d\n", min, max);
+    }
+
+    private static void findAndPrintMinMaxSecondWay(Integer[] integers) {
+        Arrays.sort(integers);
+        int min = integers[0];
+        int max = integers[integers.length - 1];
+        System.out.printf("Min = %d; Max = %d\n", min, max);
+    }
+
+    private static Integer[] generateIntArray(int size) {
+        Integer[] integers = new Integer[size];
+        RandomGenerator randomGenerator = RandomGenerator.getDefault();
+        for (int i = 0; i < size; i++) {
+            integers[i] = randomGenerator.nextInt(100);
+        }
+        return integers;
+    }
+
 }
